be2d72c504b84c09030fd01c29a856e311801886
Allen Sarmiento <allenreysarmiento@outlook.com>
2023-08-08 21:24:59 -0700

    Starter project

---
+++ src/test/java/com/allensrmnto/userauthsystem/UserAuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allensrmnto.userauthsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
